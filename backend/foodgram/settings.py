9f2b0d0e1ba92d3b9a571950e3c4d98a6f25f4e3
Artem Barsukov <iv4nst18@yandex.ru>
2024-05-20 16:10:01 +0300

    workflow11

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -164,7 +164,5 @@ DJOSER = {
     },
 }
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
